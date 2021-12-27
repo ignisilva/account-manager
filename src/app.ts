a1789e43234463e012901a74e631df71b519b469
Songhareem <ignisilva92@gmail.com>
2021-12-27 19:23:54 +0900

    fix: app functions to class

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import Router from "./core/router";
+import View from "./core/view";
 import { MainView } from "./page";
+import { Store } from "./store";
 
-// 라우터 구성
-const router: Router = new Router();
-const mainView: MainView = new MainView("root");
+class App {
+  private store: Store;
+  private router: Router;
+  private views: View[];
 
-router.setDefaultPage(mainView);
+  constructor() {
+    this.store = new Store();
+    this.router = new Router();
+    this.views = [new MainView("root", this.store)];
+  }
+
+  public run() {
+    this.router.setDefaultPage(this.views[0]);
+  }
+}
+
+const app = new App();
+app.run();
